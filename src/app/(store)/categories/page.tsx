b11038ef0887d11a61ec2ecd004545e2b2f0657f
Minhaj Arshad <itx.minhaj@gmail.com>
2025-12-22 00:43:26 +0500

    some later on changes in main

---
+++ src/app/(store)/categories/page.tsx
@@ -0,0 +1,10 @@
+//CATAGORY WALA COMPONENT BANA GA YA PHIR CATAGORY KA PAGE BANA GA
+import React from 'react'
+
+const category = () => {
+  return (
+    <div>category</div>
+  )
+}
+
+export default category
